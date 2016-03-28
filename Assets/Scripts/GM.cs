34c4c1a18cd90f7405bbd5d7fdb95b6ee9b99fe5
Redbulbul <levaricohen@gmail.com>
2016-03-28 15:33:06 +0300

    Added GM + Bricks
    Used Unity tutorial to improve code and to build correct infrastructure

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour
+{
+
+    public int lives = 3;
+    public int bricks = 4;
+    public float resetDelay = 1f;
+    public Text livesText;
+  //  public GameObject gameOver;
+  //  public GameObject youWon;
+    public GameObject bricksPrefab;
+    public GameObject paddle;
+ //   public GameObject deathParticles;
+    public static GM instance = null;
+
+    private GameObject clonePaddle;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        Setup();
+
+    }
+
+    public void Setup()
+    {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+        Instantiate(bricksPrefab, transform.position, Quaternion.identity);
+    }
+
+    void SetupPaddle()
+    {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+    }
+
+    public void DestroyBrick()
+    {
+        bricks--;
+        //  CheckGameOver();
+    }
+
+    /*
+        void CheckGameOver()
+        {
+            if (bricks < 1)
+            {
+                youWon.SetActive(true);
+                Time.timeScale = .25f;
+                Invoke("Reset", resetDelay);
+            }
+
+            if (lives < 1)
+            {
+                gameOver.SetActive(true);
+                Time.timeScale = .25f;
+                Invoke("Reset", resetDelay);
+            }
+
+        }
+    */
+
+    /*
+        public void LoseLife()
+        {
+            lives--;
+            livesText.text = "Lives: " + lives;
+            Instantiate(deathParticles, clonePaddle.transform.position, Quaternion.identity);
+            Destroy(clonePaddle);
+            Invoke("SetupPaddle", resetDelay);
+            CheckGameOver();
+        }
+    */
+
+    void Reset()
+    {
+        Time.timeScale = 1f;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("stage");
+
+      //  Application.LoadLevel(Application.loadedLevel);
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+    public int paddleSpeed = 800;
+    // private Vector3 paddlePosition = new Vector3(0, 0, 0);
+	
+	void Update () {
+        transform.Rotate(0, 0, Input.GetAxis("Horizontal") * Time.deltaTime * paddleSpeed * -1);
+    }
+}
+
+
+ 
